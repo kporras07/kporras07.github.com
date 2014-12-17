b8dd72ce92cd43d218ab1d1fc0d1fda4c225f44e
Kevin Porras <kporras07@gmail.com>
2014-12-17 16:04:57 -0600

    Changed like button style

--- assets/js/index.js
+++ assets/js/index.js
@@ -35,6 +35,6 @@
         fjs.parentNode.insertBefore(js, fjs);
     };
     load('//platform.twitter.com/widgets.js', 'tweetjs');
-    load('//connect.facebook.net/en_US/all.js#xfbml=1', 'fbjssdk');
+    load('//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=1560868307491624&version=v2.0', 'fbjssdk');
     load('https://apis.google.com/js/plusone.js', 'gplus1js');
 }(document, 'script'));
